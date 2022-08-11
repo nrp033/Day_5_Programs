909534f0b6f0ea1ef0bf5875ee6c89412adaf41c
nrp033 <pawarnitin9433@gmail.com>
2022-08-11 21:21:47 +0530

    Day 5 Basic core programs

---
+++ Day5_BasicCorePrograms/Day5_BasicCorePrograms/Even_Odd.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day5_BasicCorePrograms
+{
+    internal class Even_Odd
+    {
+        public static void show_evenodd()
+        {
+            Console.WriteLine("Enter the Number =");
+            int Num = Convert.ToInt32(Console.ReadLine());
+            if (Num % 2 == 0)
+            {
+                Console.WriteLine("Given Number is Even !");
+            }
+            else
+            {
+                Console.WriteLine("Given Number is Odd !");
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
